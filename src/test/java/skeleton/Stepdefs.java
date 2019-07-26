eea8e18d87f48ee98850666d281bf3950f45c897
sudeeptha967 <53205609+sudeeptha967@users.noreply.github.com>
2019-07-26 14:15:05 +0530

    Update Stepdefs.java

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -14,7 +14,7 @@ import cucumber.api.java.en.When;
 import junit.framework.Assert;
 
 public class Stepdefs {
-	WebDriver driver;
+	static WebDriver driver;
 	@When("open home page")
 	public void open_home_page() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Selium 3.0\\Selium 3.0\\driver\\chromedriver.exe");
@@ -23,9 +23,11 @@ public class Stepdefs {
 	}
 
 	@When("enter the username {string}")
-	public void enter_the_username(String un) {
-		driver.findElement(By.name("userName")).sendKeys(un);
-	}
+	public void enter_the_username(String uname) {
+		
+		driver.findElement(By.name("userName")).sendKeys(uname);
+		//driver.findElement(By.xpath("/html/body/main/div/div/form/fieldset/div/div[13]/div/input[2]")).click();
+		}
 	@When("enter the firstName")
 	public void enter_the_firstname() {
 		driver.findElement(By.name("firstName")).sendKeys("mercury");
@@ -81,5 +83,33 @@ public class Stepdefs {
 	@Then("click register")
 	public void click_register() {
 		driver.findElement(By.name("Submit")).click();
+	}
+	@When("enter name {string}")
+	public void enter_username(String uname) {
+	 driver.findElement(By.name("userName")).sendKeys(uname);
+	}
+	@When("enter password {string}")
+	public void enter_password(String pswd) {
+	 driver.findElement(By.name("password")).sendKeys(pswd);
+	}
+	@And("enter login button")
+	public void enter_button() {
+		 driver.findElement(By.name("Login")).click();
+		}
+
+	@Given("search the particular product")
+	public void search_the_particular_product() throws InterruptedException {
+		WebElement  search = driver.findElement(By.xpath("//*[@id=\"myInput\"]"));//By.xpath("//*[@id=\"myInput\"]")"fa fa-search"
+		Actions a =new Actions(driver);
+		a.sendKeys(search,"headphone").build().perform();
+		Thread.sleep(2000);
+		a.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
+	}
+
+	@Given("try to proceed to payment without adding any item in the cart")
+	public void try_to_proceed_to_payment_without_adding_any_item_in_the_cart() {
+		driver.findElement(By.xpath("/html/body/section/div/div/div[2]/div/div/div/div[2]/center/a")).click();
+		driver.findElement(By.partialLinkText("Cart")).click();
+		driver.findElement(By.xpath("//*[@id=\"cart\"]/tbody/tr/td[5]/form/input[2]")).click();//.cssSelector("btn btn-danger btn-sm")
 	}
 }
